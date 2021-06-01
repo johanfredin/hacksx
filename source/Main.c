1952313652023de0b23e91d7ea8d8a7f43ff9be1
Johan Fredin <johanfredin@fastmail.com>
2021-06-02 00:25:40 +0200

    fixed x and y positioning

--- source/Main.c
+++ source/Main.c
@@ -13,12 +13,10 @@ int main() {
     sprite.w = sprite.h = 256;
 
     GsSPRITE *tiles = tiles_get_gs_tile_sprites_mapped(&sprite, &map);
-    for(y = 0; y < 16; y++) {
-        for (x = 0; x < 16; x++) {
-            GsSPRITE curr = tiles[x * y];
-            printf("[x=%d, y=%d, u=%d, v=%d], ", curr.x, curr.y, curr.u, curr.v);
-        }
-        printf("\n");
+    for (i = 0; i < 256-8; i++) {
+        GsSPRITE curr = tiles[i];
+        printf("[%d, %d], ", curr.x, curr.y);
     }
+    printf("\n");
     return 0;
 }
--- source/TileFetcher.c
+++ source/TileFetcher.c
@@ -23,13 +23,21 @@ u_short to_tm_v_coord(u_short id, u_short tileset_cols, u_char curr_idx) {
 
 GsSPRITE *tiles_get_gs_tile_sprites_mapped(GsSPRITE *tile_set, Tile_Map *map) {
     u_char i, x, y;
+    u_short rows_cnt, cols_cnt;
     Layer_Data *data;
     GsSPRITE *sprites = MEM_CALLOC_3(256, GsSPRITE);
     Tile_Layer *layer = map->layers;  // Update later to parse multiple layers
     u_short tileset_rows = tile_set->h / map->tile_height;
     u_short tileset_cols = tile_set->w / map->tile_width;
 
-    for (data = layer->data, i = 0; data != NULL; data = data->next, i++) {
+    rows_cnt = 0;
+    cols_cnt = 0;
+    for (data = layer->data, i = 0; data != NULL; data = data->next, i++, cols_cnt++) {
+        if(cols_cnt >= map->tile_width) {
+            cols_cnt = 0;
+            rows_cnt += 1;
+        }
+
         u_short u, v;
         u_short id = data->id;
         if (id == 0) {
@@ -37,15 +45,14 @@ GsSPRITE *tiles_get_gs_tile_sprites_mapped(GsSPRITE *tile_set, Tile_Map *map) {
             continue;
         }
 
-        x = (i % tileset_cols) * map->tile_width;
-        y = (i % tileset_rows) * map->tile_height;
+        x = cols_cnt * map->tile_width;
+        y = rows_cnt * map->tile_height;
 
         --id;   // because they are 1 indexed in exported json, but tileset is 0 indexed
         u = map->tile_width * to_tm_u_coord(id, tileset_cols);
         v = map->tile_height * to_tm_v_coord(id, tileset_rows, i);
 
         asmg_get_region(tile_set, &sprites[i], x, y, u, v, map->tile_width, map->tile_height);
-        i++;
     }
     return sprites;
 }
